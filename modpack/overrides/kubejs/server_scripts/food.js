4d826d158425319dad503fc7695d308a8d0d2665
Gaelmare <2gaelmare@gmail.com>
2024-11-26 01:03:04 -0700

    First food changes and ProbeJS config

---
+++ modpack/overrides/kubejs/server_scripts/food.js
@@ -0,0 +1,26 @@
+
+TFCEvents.data(event => {
+    event.foodItem('ars_nouveau:sourceberry_bush', food => {
+        food.hunger(4)
+        food.fruit(1)
+        food.decayModifier(3)
+    })
+})
+
+TFCEvents.data(event => {
+    event.foodItem('ars_nouveau:source_berry_pie', food => {
+        food.hunger(4)
+        food.fruit(3)
+        food.grain(1)
+        food.decayModifier(2)
+    })
+})
+
+TFCEvents.data(event => {
+    event.foodItem('ars_nouveau:source_berry_roll', food => {
+        food.hunger(4)
+        food.fruit(.8)
+        food.grain(2.5)
+        food.decayModifier(1.5)
+    })
+})
